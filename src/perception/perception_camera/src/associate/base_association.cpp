d721578c9bea222342da3cb70a37c7999c915292
superg_tsh <sheng.tao@superg.ai>
2019-12-18 23:09:52 +0800

    slam可运行版本,下一步做lidar 定位

--- src/perception/perception_camera/src/associate/base_association.cpp
+++
@@ -1,9 +0,0 @@
-#include "associate/base_association.h"
-
-sensor_camera::BaseAssociation::BaseAssociation(int cost_threshold):cost_threshold_(cost_threshold)
-{
-}
-
-sensor_camera::BaseAssociation::~BaseAssociation()
-{
-}
